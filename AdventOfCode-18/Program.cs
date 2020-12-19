80e41139d141fa0e1167de46cda1a47d36fae00f
Wolfgang Richter <wolf@wolfgroup.llc>
2020-12-18 23:20:39 -0500

    AdventOfCode-18: final solution
    + Note to self: definitely work in postfix notation it's much easier

--- AdventOfCode-18/Program.cs
+++ AdventOfCode-18/Program.cs
@@ -7,171 +7,6 @@ namespace AdventOfCode_18
     class Program
     {
 
-
-        static string reverse(string line)
-        {
-            string return_string = "";
-            string number = "";
-            int i = line.Length - 1;
-            Console.WriteLine("Parse call");
-
-            for (; i >= 0; i--)
-            {
-                switch (line[i])
-                {
-                    case '*':
-                        return_string += '*';
-                        break;
-                    case '+':
-                        return_string += '+';
-                        break;
-                    case '(':
-                        if (number != "")
-                        {
-                            char[] arr = number.ToCharArray();
-                            Array.Reverse(arr);
-                            number = new string(arr);
-                            return_string += number;
-                            number = "";
-                        }
-                        return_string += ')';
-                        break;
-                    case '0':
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                        number += line[i];
-                        break;
-                    case ')':
-                        if (number != "")
-                        {
-                            char[] arr = number.ToCharArray();
-                            Array.Reverse(arr);
-                            number = new string(arr);
-                            return_string += number;
-                            number = "";
-                        }
-                        return_string += '(';
-                        break;
-                    case ' ':
-                        if (number != "")
-                        {
-                            char[] arr = number.ToCharArray();
-                            Array.Reverse(arr);
-                            number = new string(arr);
-                            return_string += number;
-                            number = "";
-                        }
-                        return_string += ' ';
-                        break;
-                    default:
-                        throw new Exception();
-                }
-            }
-
-            if (number != "")
-            {
-                char[] arr = number.ToCharArray();
-                Array.Reverse(arr);
-                number = new string(arr);
-                return_string += number;
-                number = "";
-            } 
-
-            return return_string;
-        }
-
-        // parses and evaluates a single expression at a time
-        static Tuple<int, int> parse(string line, bool leftmost)
-        {
-            int evaluation = 0;
-            int previous_evaluation = 0;
-            string number = "";
-            int i = 0;
-            int parsed = 0;
-            Console.WriteLine("Parse call");    
-
-            for (; i < line.Length; i++)
-            {
-                Console.WriteLine($"leftmost == {leftmost}, i == {i}, line == '{line}', line.Length == {line.Length}, line[i] == '{line[i]}', previous_evaluation == {previous_evaluation}, evaluation == {evaluation}, number == '{number}'");
-                switch(line[i])
-                {
-                    case '*':
-                        (parsed, evaluation) = parse(line.Substring(i + 1), false);
-                        Console.WriteLine($"{previous_evaluation} * {evaluation}");
-                        previous_evaluation *= evaluation;
-                        i += parsed;
-                        break;
-                    case '+':
-                        (parsed, evaluation) = parse(line.Substring(i + 1), false);
-                        Console.WriteLine($"{previous_evaluation} + {evaluation}");
-                        previous_evaluation += evaluation;
-                        i += parsed;
-                        break;
-                    case '(':
-                        if (number != "")
-                        {
-                            previous_evaluation = int.Parse(number);
-                            number = "";
-                            if (!leftmost)
-                            {
-                                Console.WriteLine($"Returning: ({i}, {previous_evaluation})");
-                                return new Tuple<int, int>(i, previous_evaluation);
-                            }
-                        }
-                        (parsed, evaluation) = parse(line.Substring(i + 1), false);
-                        previous_evaluation = evaluation;
-                        i += parsed;
-                        return new Tuple<int, int>(i, previous_evaluation);
-                        break;
-                    case '0':
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                        number += line[i];
-                        break;
-                    case ')':
-                    case ' ':
-                    case '\n':
-                    case '\r':
-                        if (number != "")
-                        {
-                            previous_evaluation = int.Parse(number);
-                            number = "";
-                            if (!leftmost)
-                            {
-                                Console.WriteLine($"Returning: ({i}, {previous_evaluation})");
-                                return new Tuple<int, int>(i, previous_evaluation);
-                            }
-                        }
-                        continue;
-                    default:
-                        throw new Exception();
-                }
-            }
-
-            if (previous_evaluation == 0 && number != "")
-            {
-                previous_evaluation = int.Parse(number);
-            }
-
-            Console.WriteLine($"Returning: ({i}, {previous_evaluation})");
-
-            return new Tuple<int, int>(i, previous_evaluation);
-        }
-
         static Stack<string> eval_expression(string[] tokens)
         {
             Stack<string> stack = new Stack<string>();
@@ -240,6 +75,157 @@ namespace AdventOfCode_18
             return stack;
         }
 
+        static Stack<string> eval_expression2(string[] tokens)
+        {
+            Stack<string> stack = new Stack<string>();
+            Stack<string> next = null;
+            string operation = null;
+            int parens = 0;
+
+            foreach (string token in tokens)
+            {
+                switch (token)
+                {
+                    case "+":
+                        operation = token;
+                        stack.Push(operation);
+                        break;
+                    case "*":
+                        operation = token;
+                        stack.Push(operation);
+                        break;
+                    case "(":
+                        parens++;
+                        stack.Push(token);
+                        operation = null;
+                        break;
+                    case ")":
+                        List<string> parens_tokens = new List<string>();
+                        while (stack.Peek() != "(")
+                        {
+                            parens_tokens.Add(stack.Pop());
+                        }
+                        string[] tokens_arr = parens_tokens.ToArray();
+                        Array.Reverse(tokens_arr);
+                        next = eval_expression2(tokens_arr);
+                        stack.Pop();
+                        foreach (string val in next)
+                        {
+                            stack.Push(val);
+                        }
+                        parens--;
+                        operation = null;
+                        break;
+                    default:
+                        if (operation == null || parens != 0 || stack.Count > 2)
+                        {
+                            stack.Push(token);
+                        }
+                        else
+                        {
+                            switch (operation)
+                            {
+                                case "+":
+                                    stack.Pop();
+                                    stack.Push((long.Parse(stack.Pop()) + long.Parse(token)).ToString());
+                                    break;
+                                case "*":
+                                    stack.Pop();
+                                    stack.Push((long.Parse(stack.Pop()) * long.Parse(token)).ToString());
+                                    break;
+                                default:
+                                    throw new Exception();
+                            }
+                            operation = null;
+                        }
+                        break;
+                }
+            }
+            return stack;
+        }
+
+        static int precedence(string token)
+        {
+            switch (token)
+            {
+                case "+":
+                    return 2;
+                case "*":
+                    return 1;
+                default:
+                    return -1;
+            }
+        }
+
+        static List<string> to_postfix(string[] tokens)
+        {
+            List<string> return_tokens = new List<string>();
+
+            Stack<string> stack = new Stack<string>();
+
+            foreach (string token in tokens)
+            {
+                switch (token)
+                {
+                    case "*":
+                    case "+":
+                        while (stack.Count > 0 && precedence(token) <= precedence(stack.Peek()))
+                        {
+                            return_tokens.Add(stack.Pop());
+                        }
+                        stack.Push(token);
+                        break;
+                   case "(":
+                        stack.Push(token);
+                        break;
+                    case ")":
+                        while (stack.Peek() != "(")
+                        {
+                            return_tokens.Add(stack.Pop());
+                        }
+                        stack.Pop();
+                        break;
+                    default:
+                        return_tokens.Add(token);
+                        break;
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                return_tokens.Add(stack.Pop());
+            }
+
+            return return_tokens;
+        }
+
+        static long postfix_eval(List<string> tokens)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            foreach (string token in tokens)
+            {
+                switch(token)
+                {
+                    case "*":
+                        long lhs = long.Parse(stack.Pop());
+                        long rhs = long.Parse(stack.Pop());
+                        stack.Push((lhs * rhs).ToString());
+                        break;
+                    case "+":
+                        long lhs2 = long.Parse(stack.Pop());
+                        long rhs2 = long.Parse(stack.Pop());
+                        stack.Push((lhs2 + rhs2).ToString());
+                        break;
+                    default:
+                        stack.Push(token);
+                        break;
+                }
+            }
+
+            return long.Parse(stack.Pop());
+        }
+
         static void Main(string[] args)
         {
             string fileName = @"C:\Users\wolfg\source\repos\AdventofCode2020\AdventofCode-18\input-18.txt";
@@ -253,9 +239,6 @@ namespace AdventOfCode_18
 
             while ((line = file.ReadLine()) != null)
             {
-                //line = "1 + 2 * (3 + 4) * (5 + 6)";
-                //line = "((2) * (2) + 1) * (1 + 1)";
-                //line = "(9 + 9 + (3 * 6 + 6 * 2) * 9 + 4) * (7 * 4 + 3) * 4 + 3 + 9";
                 line = line.Replace("(", "( ");
                 line = line.Replace(")", " )");
                 string[] tokens = line.Split();
@@ -266,10 +249,27 @@ namespace AdventOfCode_18
                     Array.Reverse(tokens);
                 }
                 evaluations.Add(long.Parse(returnstack.Pop()));
-                //break;
             }
 
             long sum = 0;
+            foreach (long eval in evaluations)
+            {
+                sum += eval;
+            }
+
+            Console.WriteLine($"Total sum of evaluations = {sum}");
+
+            file = new System.IO.StreamReader(fileName);
+            evaluations = new List<long>();
+            while ((line = file.ReadLine()) != null)
+            {
+                line = line.Replace("(", "( ");
+                line = line.Replace(")", " )");
+                string[] tokens = line.Split();
+                evaluations.Add(postfix_eval(to_postfix(tokens)));
+            }
+
+            sum = 0;
             foreach (long eval in evaluations)
             {
                 sum += eval;
