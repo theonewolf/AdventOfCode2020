a460595d8f9eec08afe41cb06ec823f45345ea9f
Wolfgang Richter <wolf@wolfgroup.llc>
2020-12-23 18:31:38 -0500

    AdventOfCode-24: preparation

---
+++ AdventOfCode-24/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode_24
+{
+
+    class Program
+    {
+        static void part1()
+        {
+            string fileName = @"C:\Users\wolfg\source\repos\AdventofCode2020\AdventofCode-24\input-24-test.txt";
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            
+            Console.WriteLine(fileName);
+            Console.WriteLine(file);
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+            }
+        }
+
+        static void part2()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            part1();
+            part2();
+        }
+    }
+}
