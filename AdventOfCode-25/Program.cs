c98da0815aa6552ae5aab865a086807364d0d35f
Wolfgang Richter <wolf@wolfgroup.llc>
2020-12-24 09:04:19 -0500

    AdventOfCode-25: prep for Day 25!
    Merry Christmas Eve!

---
+++ AdventOfCode-25/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace AdventOfCode_25
+{
+
+    class Program
+    {
+
+        static void part1()
+        {
+            string fileName = @"C:\Users\wolfg\source\repos\AdventofCode2020\AdventofCode-25\input-25-test.txt";
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+
+            Console.WriteLine(fileName);
+            Console.WriteLine(file);
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                while (!string.IsNullOrEmpty(line))
+                {
+                }
+            }
+
+            static void part2()
+            {
+                string fileName = @"C:\Users\wolfg\source\repos\AdventofCode2020\AdventofCode-25\input-25-test.txt";
+                System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+
+                Console.WriteLine(fileName);
+                Console.WriteLine(file);
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                part1();
+                //part2();
+            }
+        }
+    }
+}
